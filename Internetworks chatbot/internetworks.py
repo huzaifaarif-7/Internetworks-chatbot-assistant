0992c6700e9251d5bf70562f25a64c1a35d4adf5
Muhammad Huzaifa Arif <huzaifa.pk12@gmail.com>
2025-10-01 15:21:29 +0500

    Update internetworks.py

--- Internetworks chatbot/internetworks.py
+++ Internetworks chatbot/internetworks.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key="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" )
+    api_key="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" )
 
 
 knowledge_base = """ Company Profile - INTERNETWORKS . 
